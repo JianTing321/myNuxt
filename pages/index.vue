972f543eedd054d7fd5caee56074650bb9627323
JianTing <jason717011@eip.chuanhwa.com.tw>
2022-11-29 22:11:37 +0800

    FirstCommit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+<button>{{counter}}</button>
+  </div>
+</template>
+<script lang="ts">
+
+import { useMainStore } from '../stores/main'
+import { storeToRefs } from 'pinia'
+export default defineComponent({
+  setup() {
+    const main = useMainStore()
+    // extract specific store properties
+    const { counter, doubleCounter } = storeToRefs(main)
+    return {
+      // gives access to the whole store in the template
+      main,
+      // gives access only to specific state or getter
+      counter,
+      doubleCounter,
+    }
+  },
+})
+</script>
