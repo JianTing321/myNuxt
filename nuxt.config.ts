a585def97db11db0cd983752780df2289a558cb8
JianTing321 <jason717011@gmail.com>
2022-12-03 17:34:11 +0800

    小修改

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
 	modules: [
 		[
 			// 設定掛載pinia並且將其中的工廠函數defineStore,defineStore自動載入自個別頁面(包含store的main),無須再引入
-			'@pinia/nuxt', { autoImports: ['defineStore', 'defineStore'] }],
+			'@pinia/nuxt', { autoImports: ['defineStore', 'storeToRefs'] }],
 		[
 			'@nuxtjs/tailwindcss'
 		]
