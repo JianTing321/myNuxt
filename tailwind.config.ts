50759d9f783705894d015e0552950455a42ea4bf
JianTing321 <jason717011@gmail.com>
2022-12-18 20:02:34 +0800

    taiwind css增加可全局自定義配置

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,15 @@
 import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
+  // 會自動偵測 在打包出去後沒用到taiwindcss的頁面不會有taiwindcss
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './app.vue'
+  ],
   theme: {
     // 自訂義顏色 名稱也可自定義(需要重新build顏色才會套用)
     extend: {
@@ -26,5 +35,6 @@ export default <Partial<Config>>{
         }
       }
     }
-  }
+  },
+  plugins: []
 }
