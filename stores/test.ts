d44ea8c75a31883e29eecd2b036e76ebe9f83809
JianTing321 <jason717011@gmail.com>
2023-02-06 09:08:21 +0800

    test.ts加入interface

--- stores/test.ts
+++ stores/test.ts
@@ -1,9 +1,15 @@
 // import { defineStore } from 'pinia'
 // main is the name of the store. It is unique across your application
 // and will appear in devtools
+
+interface test {
+  set:string,
+  testSet:number
+}
+
 export const useTestStore = defineStore('test', {
   // a function that returns a fresh state
-  state: () => ({
+  state: ():test => ({
     set: '測試的倉庫',
     testSet: 222
   }),
